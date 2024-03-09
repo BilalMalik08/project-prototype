5c0d4215500c36d54b137d9583f7934b949c59bb
BilalMalik08 <muhammad.billal.cs@gmail.com>
2024-03-09 14:36:57 +0500

    Updated 2:30 PM, 9th March, 2024

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,14 +1,32 @@
 import "./footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+// Custom component to open links in new tab
+const ExternalLink = ({ to, children }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.open(to, "_blank");
+  };
+
+  return <span onClick={handleClick}>{children}</span>;
+};
 
 function Footer() {
   return (
-    <>
-      <div className="row footer-row">
-        <h6 className="footer-h6">
-          Copyright © www.equalityimmigration.com | All rights reserved!
-        </h6>
+    <div className="footer-row">
+      <h6 className="footer-h6">
+        Copyright © www.equalityimmigration.com | All rights reserved!
+      </h6>
+      <div className="social-links">
+        <ExternalLink to="https://www.facebook.com">
+          <FontAwesomeIcon icon={faFacebook} />
+        </ExternalLink>
+        <ExternalLink to="https://www.instagram.com/equalityimmigration.ie?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+          <FontAwesomeIcon icon={faInstagram} />
+        </ExternalLink>
       </div>
-    </>
+    </div>
   );
 }
 
